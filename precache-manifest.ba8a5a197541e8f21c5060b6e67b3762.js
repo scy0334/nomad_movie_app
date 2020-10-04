d96947444e5949c806c5ecf3fca66cdd8bf83712
Si Choi <0334pc@gmail.com>
2020-10-04 11:44:49 +0900

    Updates

---
+++ precache-manifest.ba8a5a197541e8f21c5060b6e67b3762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a5713cb95de75271eb52b7f05c555d1",
+    "url": "/nomad_movie_app/index.html"
+  },
+  {
+    "revision": "65df001eeeb518d9316b",
+    "url": "/nomad_movie_app/static/css/main.0dd8a67b.chunk.css"
+  },
+  {
+    "revision": "d3abd1aac27abc680fa9",
+    "url": "/nomad_movie_app/static/js/2.f5afd354.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nomad_movie_app/static/js/2.f5afd354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65df001eeeb518d9316b",
+    "url": "/nomad_movie_app/static/js/main.77fd278f.chunk.js"
+  },
+  {
+    "revision": "f6bd22143c1cc95f4ec8",
+    "url": "/nomad_movie_app/static/js/runtime-main.4d1f4ffb.js"
+  }
+]);
